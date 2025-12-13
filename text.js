862d1fb6dfaeb6671dde8b580006fe9cb6d53013
Confidence <confi2s7@gmail.com>
2025-12-13 12:05:34 +0100

    jj

--- text.js
+++ text.js
@@ -4,48 +4,86 @@ const WelcomeMessage = document.getElementById("WelcomeMessage"),
 countDown = document.getElementById("countDown"),
 countingnumber = document.getElementById("countingnumber"),
 bouncingdot = document.getElementById("bouncingdot"),
-speedTexting = document.getElementById("speedTexting")
+speedTexting = document.getElementById("speedTexting"),
+userTyped = document.getElementById("userTyped"),
+timerDisplay = document.getElementById("timer"),
+takeTest = document.getElementById("takeTest")
 
-// setting timeout for opacity
 
-WelcomeMessage.classList.add("welcomeOpacity")
-setTimeout(() => {
-    WelcomeMessage.classList.remove("welcomeOpacity")
-}, 900);
+if(WelcomeMessage){
+    // setting timeout for opacity
+    WelcomeMessage.classList.add("welcomeOpacity")
+    setTimeout(() => {
+        WelcomeMessage.classList.remove("welcomeOpacity")
+    }, 1000);
 
+    
 /* setting time out for both
 countDown and WelcomeMessage */
+
 setTimeout(() => {
     WelcomeMessage.style.display = "none"
-}, 6000);
-setTimeout(() => {
     countDown.style.display = "flex"
-}, 6500);
-
+    StartAfterWelcome()
+}, 6000);
+}
 
 
 // stting time out for my countDown numbers 3,2,1
 
-setTimeout(() => {
-    setTimeout(() => {
-        countingnumber.innerText  = '3'
-    }, 3000);
-    setTimeout(() => {
-        countingnumber.innerText  = '2'
-    }, 4000);
-    setTimeout(() => {
-        countingnumber.innerText  = "1"  
-    }, 6000);
-    setTimeout(() => {
-        countDown.style.display = "none"
-    }, 6600);
-    setTimeout(() => {
-        speedTexting.style.display = "flex"
-    }, 6700);
-}, 7000);
+let timeReady = 3;
+let startedReday = false;
+let intervalRea;
+function StartAfterWelcome(params) {
+    intervalRea  = setInterval(() => {
+        countingnumber.textContent = timeReady
+        timeReady--
+        if(timeReady < 0){
+            countDown.style.display = "none"
+            speedTexting.style.display = "flex"
+        }
+    }, 1000);
+}
+
 
 // adding my animation for the counting Numbers
 setTimeout(() => {
     countDown.classList.add('transfrom')
 }, 100);
 
+
+
+// countDown
+
+let time = 3;
+let started = false;
+let interval;
+function CountDown() {
+
+    if (started) return; // prevent multiple intervals
+    started = true;
+  
+    interval = setInterval(() => {
+        timerDisplay.textContent = time;
+      time--;
+  
+      if (time < 0) {
+        clearInterval(interval);
+        userTyped.disabled = true; // stop typing
+      }
+    }, 1000);
+}   
+
+userTyped.addEventListener("input", CountDown);
+
+takeTest.addEventListener('click', function (params) {
+    if(time === -1){
+        clearInterval(interval);
+        time = 3;
+        timerDisplay.textContent = time;
+        started = false;
+        userTyped.disabled = false
+        countDown()
+        userTyped.value = '';
+    }
+})
