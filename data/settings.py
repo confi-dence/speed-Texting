d0b4482c48c1afdc14f0d362a41273680d8f68eb
Confidence <confi2s7@gmail.com>
2026-01-26 19:21:29 +0100

    Prepare project for Render deployment

--- data/settings.py
+++ data/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0sy#+%ryxt!mo2*9!5px%i)p705desz_5p&z33patd8j5#ta*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
